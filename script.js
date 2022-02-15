e99208a8395ef1ce738363589d0f5e972eb36400
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2022-02-15 19:16:30 +0200

    arranging code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var key = "d87172a12149bbe6225de4fd765bf31f";
-document.getElementById("submitButton").addEventListener("click", displayChart);
-var chart = document.querySelector("#chart");
+var key = "274f4e06d8eb0e2f1da56e878fdcd83a"; // private API_KEY
+document.getElementById("submitButton").addEventListener("click", displayChart); // to display chart on click
+var chart = document.querySelector("#chart"); // chart section
 
+// chart customization
 var options = {
   chart: {
     type: "candlestick",
@@ -27,9 +28,8 @@ var options = {
 };
 
 function displayChart() {
-  var symbol = document.getElementById("symbol-input").value;
-  //getting data from API and creating the chart
-  get(symbol);
+  var symbol = document.getElementById("symbol-input").value; // getting the symbol entered by user
+  get(symbol); //getting data from API and creating the chart
 }
 
 function get(symbol) {
@@ -47,37 +47,39 @@ function get(symbol) {
         );
         return;
       }
-      // Examine the text in the response
+
       response.json().then(function (data) {
-        console.log(data);
-        formatData(data, symbol);
-        function createChart() {
-          chart.innerText = "";
-          chart = new ApexCharts(document.querySelector("#chart"), options);
-          chart.render();
-        }
-        createChart();
+        console.log(data); //displaying data from API in console
+        formatData(data, symbol); //formatting data to put in chart
+        createChart(); //creating the chart
       });
     })
+
     .catch(function (err) {
       console.log("Fetch Error :-S", err);
     });
 }
 
 function formatData(stockData, symbol) {
-  options.title.text = symbol;
+  options.title.text = symbol; //chart's title
   let solution = [];
   var apiData = stockData.data;
   for (var entry of apiData) {
     let obj = {};
-    obj.x = new Date(entry.date); //the x axis will be containing the date
+    obj.x = new Date(entry.date); // the x axis will be containing the date
     let price = [];
     price.push(entry.open);
     price.push(entry.high);
     price.push(entry.low);
     price.push(entry.close);
-    obj.y = price; //the y axis will be containing the prices
-    solution.push(obj);
+    obj.y = price; // the y axis will be containing the prices
+    solution.push(obj); //adding object to array
   }
-  options.series[0].data = solution.sort((a, b) => a.x - b.x); //put in series what we obtained
+  options.series[0].data = solution; // adding in chart's array the info we got
+}
+
+function createChart() {
+  chart.innerText = "";
+  chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
 }
